dfd8339974bd47853bbdce771befbabb37fd3c97
Leslie <mslesliewu@gmail.com>
2017-09-26 14:32:48 -0700

    added a books and authors app

--- surjango/urls.py
+++ surjango/urls.py
@@ -17,5 +17,5 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('apps.dojo_ninjas.urls')),
+    url(r'^', include('apps.book_authors.urls')),
 ]
